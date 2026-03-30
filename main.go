02e6bdd9e21ad23a3b4cf5e9801e443f74aa520f
dissolvdgrl <c@chilldsgn.com>
2026-03-30 20:47:08 +0200

    wip: grab files to pass to the parser

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"log"
+)
 
 func main() {
-	fmt.Println("Dmarc Observer build in progress")
+	fmt.Println("Dmarc Observer build in progress...")
+	files, err := os.ReadDir("xml-reports")
+
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,4 +1,11 @@
+// Package that parses an XML DMARC report and returns its structure
+
 package parser
+
+import (
+	"encoding/xml"
+	"io"
+)
 
 type DateRange struct {
 	Begin int64 `xml:"begin"`
@@ -20,12 +27,61 @@ type PolicyPublished struct {
 	P string `xml:"p"` // policy: none, quarantine, reject
 	Sp string `xml:"sp"` // policy for subdomains: same as above, fallback to p if absent
 	Pct uint8  `xml:"pct"` // % of mail policy applies to
-	Np string `xml:"np"` // policy for non-existent subdomains: same values as above	
+	Np string `xml:"np"` // policy for non-existent subdomains: same values as above
 }
 
+type PolicyEvaluated struct {
+	Disposition string `xml:"disposition"`
+	Dkim string `xml:"dkim"`
+	Spf string `xml:"spf"`
+}
+
+type Row struct {
+	SourceIp string `xml:"source_ip"`
+	Count uint8 `xml:"count"`
+	PolicyEvaluated PolicyEvaluated `xml:"policy_evaluated"`
+}
+
+type Identifiers struct {
+	HeaderFrom string `xml:"header_fromt"`
+}
+
+type Dkim struct {
+	Domain string `xml:"domain"`
+	Result string `xml:"pass"`
+	Selector string `xml:"selector"`
+}
+
+type Spf struct {
+	Domain string `xml:"domain"`
+	Result string `xml:"result"`
+}
+
+type AuthResults struct {
+	Dkim Dkim `xml:"dkim"`
+	Spf Spf `xml:"spf"`
+}
+
+type Record struct {
+	Row Row `xml:"row"`
+	Identifiers Identifiers `xml:"identifiers"`
+}
 
 type Feedback struct {
 	Version string `xml:"version"`
 	ReportMetaData ReportMetaData `xml:"report_metadata"`
 	PolicyPublished PolicyPublished `xml:"policy_published"`
+	Record Record `xml:"record"`
+}
+
+func ParseReport(r io.Reader) (*Feedback, error) {
+	var report Feedback
+	decoder := xml.NewDecoder(r)
+	error := decoder.Decode(&report)
+
+	if error != nil {
+		return nil, error
+	}
+
+	return &report, nil
 }
